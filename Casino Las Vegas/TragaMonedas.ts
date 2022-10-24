e49eb887e63e4a41fffa610dbbc4d9d47a979b6a
Melina Figueroa <melinafigueroa@tdf.edu.ar>
2022-10-23 23:29:23 -0300

    ingreso de metodos en clase tragamonedas

--- Casino Las Vegas/TragaMonedas.ts
+++ Casino Las Vegas/TragaMonedas.ts
@@ -1,8 +1,33 @@
 import { Casino } from "./Casino";
 import { Juego } from "./Juego";
 export class TragaMonedas extends Casino implements Juego{
-    protected tipoDeJuego: Juego;
+    protected tipoDeJuego: string;
+    constructor(pTipoDeJuego:string,pNombre:string,pCantidadFichas:number){
+        super(pNombre,pCantidadFichas);
+        this.tipoDeJuego = pTipoDeJuego;
+    }
+    
+    ingresarTipoDeJuego(pTipoDeJuego: string): void {
 
-    constructor(pTipoDeJuego:Juego)
+    }
+
+    obtenerTipoDeJuego(): string {
+
+    }
+
+    jugar(pTipoDeJuego: string): void {
+
+    }
+
+    ingresarApuesta(paramFichas: number) {
+
+
+    }
+
+    calcularApuesta() {
+
+    }
+
+    
 
 }
