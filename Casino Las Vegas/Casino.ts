db7cffac21b8be155a84653382931a7d61557800
Marcosfarina2022 <marcosfarina@tdf.edu.ar>
2022-10-23 22:43:01 -0300

    creación de clases casino y clientes

--- Casino Las Vegas/Casino.ts
+++ Casino Las Vegas/Casino.ts
@@ -1,8 +1,17 @@
-export class Casino{
+export abstract class Casino{
 
+protected nombreCasino:string;
+protected cantFichas:number;
+constructor(paramNombreCasino:string, paramFichas:number){
+    this.nombreCasino= paramNombreCasino;
+    this.cantFichas=paramFichas;
+}
 
+abstract ingresarApuesta(paramFichas:number);
+abstract calcularApuesta()
 
+public getNombre():string{
+    return this.nombreCasino;
+}
 
-
-    
 }
---
+++ Casino Las Vegas/Cliente.ts
@@ -0,0 +1,21 @@
+export class Cliente{
+
+protected nombreJugador:string;
+protected dniJugador: number;
+constructor(paramNombreJugador:string,paramDni:number){
+    this.nombreJugador=paramNombreJugador;
+    this.dniJugador=paramDni;
+}
+public getNombreJugador():string{
+    return this.nombreJugador;
+}
+public setNombreJugador(paramNombreJugador:string){
+    this.nombreJugador=paramNombreJugador;
+}
+public getDniJugador():number{
+    return this.dniJugador;
+}
+public setDniJugador(paramDni:number){
+    this.dniJugador=paramDni;
+}
+}
