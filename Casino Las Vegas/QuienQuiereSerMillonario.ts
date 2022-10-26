dcbd1e69af5d4e18681493e7ae0a5caf6a7be67e
Melina Figueroa <melinafigueroa@tdf.edu.ar>
2022-10-26 20:21:46 -0300

    se creo clase hija quien quiere ser millonario que hereda de traga monedas

---
+++ Casino Las Vegas/QuienQuiereSerMillonario.ts
@@ -0,0 +1,21 @@
+import { Cliente } from "./Cliente";
+import { TragaMonedas } from "./TragaMonedas";
+
+export class QuienQuiereSerMillonario extends TragaMonedas{
+    private creditoMinimo:number;
+    private doblarApuesta:number;
+    constructor(pDoblarApuesta:number,pCreditoDisponible:number,pJugador:Cliente,pFichasCompradas:number){
+        super(pCreditoDisponible,pJugador,pFichasCompradas);
+        this.creditoMinimo = 2000;
+        this.doblarApuesta = pDoblarApuesta;
+    }
+
+ 
+    public jugar(): void {
+        console.log("Esta jugando");
+    }
+    public calcularGanancia(): void {
+        console.log(this.calcularGanancia());
+    }
+    
+}
