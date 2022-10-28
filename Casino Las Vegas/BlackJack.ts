a044fbfcba0ec4223e015ba7ba15af46b6f5eb9c
Melina Figueroa <melinafigueroa@tdf.edu.ar>
2022-10-28 15:41:18 -0300

    agregue obtenerCredito

--- Casino Las Vegas/BlackJack.ts
+++ Casino Las Vegas/BlackJack.ts
@@ -1,11 +1,14 @@
+import { Casino } from "./Casino";
 import { Carta } from "./Carta";
 export class BlackJack{
-    private cartas = Carta[];
+    private cartas: Carta;
     private jugada:number[];
     constructor(){
-        this.cartas = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
     }
 
+    public obtenerCredito():number{
+        return this.comprarCredito;
+    }
     public azar(){
 
     }
