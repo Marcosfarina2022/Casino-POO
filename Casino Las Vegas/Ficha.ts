52d27c5ff27bc8d7b84f3a8a84ff0b26eb49fbee
Melina Figueroa <melinafigueroa@tdf.edu.ar>
2022-10-28 15:47:48 -0300

    se agrego objeto ficha

---
+++ Casino Las Vegas/Ficha.ts
@@ -0,0 +1,10 @@
+export class Ficha{
+private valorDeFicha: number;
+constructor(){
+    this.valorDeFicha = 15;
+    }
+     public obtenerValor(): number{
+        return this.valorDeFicha;
+
+}
+}
