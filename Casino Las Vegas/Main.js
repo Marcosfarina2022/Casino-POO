c002cbd2b6a53552c90d8516fa373a48792395a7
Marcosfarina2022 <marcosfarina@tdf.edu.ar>
2022-11-01 21:17:55 -0300

    Agregar Librería

--- Casino Las Vegas/Main.js
+++ Casino Las Vegas/Main.js
@@ -2,5 +2,13 @@
 exports.__esModule = true;
 var casino_1 = require("./casino");
 var figlet = require('figlet');
+figlet(' Gracias!!! \n\n Casino Las Vegas!!', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
 var casino = new casino_1.Casino();
 casino.ingresar();
--- Casino Las Vegas/Main.ts
+++ Casino Las Vegas/Main.ts
@@ -2,7 +2,7 @@ import { Casino } from "./casino";
 var figlet = require('figlet');
 
 
-figlet('CASINO LAS VEGAS!!!!!!', function(err, data) {
+figlet(' Gracias!!! \n\n Casino Las Vegas!!', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
