d97d2c95ce8a198b34bf60c8856be2adc3c88698
Melina Figueroa <melinafigueroa@tdf.edu.ar>
2022-11-05 01:04:34 -0300

    se agrego colors a txt en main

--- Casino Las Vegas/Main.js
+++ Casino Las Vegas/Main.js
@@ -2,6 +2,7 @@
 exports.__esModule = true;
 var casino_1 = require("./casino");
 var figlet = require('figlet');
+var colores = require('colors');
 figlet(' Gracias!!! \n\n Casino Las Vegas!!', function (err, data) {
     if (err) {
         console.log('Something went wrong...');
@@ -12,5 +13,5 @@ figlet(' Gracias!!! \n\n Casino Las Vegas!!', function (err, data) {
 });
 var casino = new casino_1.Casino();
 var nuevotexto = new casino_1.Casino();
-console.log(nuevotexto.leerArchivo('Casino.txt'));
+console.log(colores.brightGreen(nuevotexto.leerArchivo('Casino.txt')));
 casino.ingresar();
--- Casino Las Vegas/Main.ts
+++ Casino Las Vegas/Main.ts
@@ -1,6 +1,6 @@
 import { Casino } from "./casino";
 var figlet = require('figlet');
-
+var colores = require('colors');
 
 figlet(' Gracias!!! \n\n Casino Las Vegas!!', function(err, data) {
     if (err) {
@@ -12,7 +12,7 @@ figlet(' Gracias!!! \n\n Casino Las Vegas!!', function(err, data) {
 });
 let casino:Casino = new Casino();
 let nuevotexto: Casino = new Casino();
-console.log(nuevotexto.leerArchivo('Casino.txt'));
+console.log(colores.brightGreen(nuevotexto.leerArchivo('Casino.txt')));
 
 casino.ingresar();
 
